7c396fbb2968a0377f5a4b093491841838c3d4aa
scorfu <sergiucorfu@yahoo.com>
2024-02-27 05:52:19 +0200

    add module function

--- starter/index.js
+++ starter/index.js
@@ -46,21 +46,9 @@ const fs = require('fs');
 //Server
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate'); //import the exported function from replaceTemplate.js
 
 //readFileSync = sync, and you can do it bcs you are in top level. If the bellow readFile's would have been in the createServer callback fn it could not be done as in createServer the callback function gets executed each time there is a request
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/%PRODUCTNAME%/, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
